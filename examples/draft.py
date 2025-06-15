e540a1760518b47e873680db53762ce98f45ea91
sijieaaa <wang1679@e.ntu.edu.sg>
2025-06-15 11:17:49 +0800

    Update

--- examples/draft.py
+++ examples/draft.py
@@ -1,9 +1,31 @@
 import sys
 class A:
-    def main(input_str: str):
+    def main(input_str: str, input_list: list):
         print("entered main()")
-        kb_input = input()
-        print(f"kb_input: {kb_input}")
-        kb_sysstdin = sys.stdin.read()
-        print(f"kb_sysstdin: {kb_sysstdin}")
-        return "this_is_a_return_string"
+        keyboard_input = input()
+        print(f"keyboard_input: {keyboard_input}")
+        keyboard_sysstdin = sys.stdin.read()
+        print(f"keyboard_sysstdin: {keyboard_sysstdin}")
+        return "this_is_a_return_string", input_list
+
+
+
+question = """
+```
+
+```
+
+
+
+I will call a function in this code with the following information:
+stdin="thisisstdin"
+entry_function_kwargs={"input_str":"INPUT", "input_list": [1, 2, 3]}
+entry_function_name="A.main"
+
+Questions:
+What is the exact content printed in the terminal? Write your answer in ```answer_output```
+`
+
+What is the return value of the entry function? Keep in Python format. Write your answer in ```answer_return```
+
+"""
